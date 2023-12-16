151f6eee7140639d16ff011e246a0148ae15c14d
charan <chinnababu.vanarasi@gmail.com>
2023-12-17 00:06:47 +0530

    updated title and key names for mangalinks collections

--- utils/manga_links.py
+++ utils/manga_links.py
@@ -1,29 +1,54 @@
+import re
 import pandas as pd
 
-from utils.common_functions import delete_collection_records
-from utils.database_connection import get_collection
+try:
+    from utils.common_functions import delete_collection_records
+    from utils.database_connection import get_collection
+except ModuleNotFoundError:
+    from common_functions import delete_collection_records
+    from database_connection import get_collection
+
+import pandas as pd
+from urllib.parse import urlparse
 
 
 def insert_links_from_csv(filepath, collection):
     df = pd.read_csv(filepath)
+
+    bulk_operations = []
     for i, row in df.iterrows():
         link = row["links"]
-        title = link.split("/")[-2].replace("-", " ")
-        if title[-2:] == '01':
-            title = title.replace('01', '')
+        path_segments = link.split("/")
+
+        # Extract title and site
+        title_var = re.sub(r"[^a-zA-z0-9]", " ", path_segments[-2])
+        title_var = title_var.replace("01", "") if title_var[-2:] == "01" else title_var
+
+        title = " ".join(
+            [
+                word.capitalize() if len(word) > 1 else word
+                for word in title_var.split(" ")
+            ]
+        )
+
+        site = path_segments[2]
+
+        data = {"Title": title, "Site": site, "Manga_url": link}
+        # Check existence of the title in the collection
+        existing_doc = collection.find_one({"Title": title})
+        if not existing_doc:
+            bulk_operations.append(data)
+            print(f"Inserted: {i}, {link}")
         else:
-            title = title
-        site = link.split("/")[2]
-        data = {"Title": title, "Site": site, "url": link}
-        doc = collection.find_one(data)
-        if not doc:
-            print(f'inserted: {i}, {link}')
-            collection.insert_one(data)
-        else:
-            print(f'not inserted: {link}, {title}')
+            print(f"Not Inserted: {link}, {title}")
+
+    if bulk_operations:
+        collection.insert_many(bulk_operations)
 
 
 collection_name = get_collection("get_manga_links")
-fileinput = "/media/charan/code/Myprojects/PythonProjects/Linkifinity/csvfiles/links.csv"
+fileinput = (
+    "/media/charan/code/Myprojects/PythonProjects/Linkifinity/csvfiles/links.csv"
+)
 insert_links_from_csv(fileinput, collection=collection_name)
-delete_collection_records(collection_name)
+# delete_collection_records(collection_name)
