dd80067e3f81b4fcaf9f5b9caf4eeba28d813cbe
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-10-27 22:15:54 +0530

    updated callable function: run_functions.py

--- utils/run_functions.py
+++ utils/run_functions.py
@@ -4,14 +4,14 @@ from utils.manga_details import manga_details_function
 from utils.manga_links import manga_links_function
 
 # running csvlinks function
-print('----------------Started CSV Data-----------------------------')
+print('---------------------------Started CSV Data-------------------------------------')
 csv_links_function(0)
-print('----------------Started LINKS Data---------------------------')
+print('---------------------------Started LINKS Data-----------------------------------')
 # running manga links function
 manga_links_function()
-print('----------------Started DETAILS Data-------------------------')
+print('---------------------------Started DETAILS Data---------------------------------')
 # running manga details function
 manga_details_function()
-print('----------------Started CHAPTERS Data------------------------')
+print('---------------------------Started CHAPTERS Data--------------------------------')
 # running manga chapters function
 manga_chapters_function()
